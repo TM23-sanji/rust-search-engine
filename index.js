f4c3a18634a15ecd1b6f2be1fca03e6aec4f2c5f
TM23-sanji <tusharmishra802@gmail.com>
2025-05-30 17:58:55 +0530

    Frontend

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-console.log("Querying API...")
+async function search(prompt) {
+    let results = document.getElementById("results");
+    results.innerHTML = "";
+    console.log("Querying API...")
+    const response = await fetch("/api/search", {
+        method: "POST",
+        headers: {'Content-Type': 'text/plain'},
+        body: prompt,
+    });
+    for ([path, rank] of await response.json()){
+        let item = document.createElement("span");
+        item.appendChild(document.createTextNode(path));
+        item.appendChild(document.createElement("br"));
+        results.appendChild(item);
+    }
+}
 
-fetch("api/search", {
-    method: 'POST',
-    // mode: 'cors',
-    // cache: 'no-cache',
-    // credentials: 'same-origin',
-    headers: {
-        'Content-Type': 'text/plain'
-    },
-    // redirect: 'follow',
-    // referrerPolicy: 'no-referrer',
-    body: "glsl function for linearly interpolation"
-}).then((response) => console.log(response))
+let query = document.getElementById("query");
+query.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        search(query.value);
+    }
+});
